6bfae28a413860b82f95eca565072da11ddd3eee
Prrasad91 <74461952+Prrasad91@users.noreply.github.com>
2025-10-04 20:44:20 +0530

    enchanced

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # WhatsApp configuration
-WHATSAPP_NUMBER = "+919786496869"  # Your WhatsApp number with country code
+WHATSAPP_NUMBER = ""  # Your WhatsApp number with country code
 
 # OpenAI configuration
-OPENAI_API_KEY = "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"  # Get this from platform.openai.com
+OPENAI_API_KEY = "sk-proj--"  # Get this from platform.openai.com
 
 # Trading parameters
 MIN_TRADE_SCORE = 3  # Minimum score required to generate alert (0-5)
